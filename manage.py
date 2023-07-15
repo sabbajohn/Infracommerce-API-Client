2761524694c2bfab9c5e919df75c6123110c47c1
SabbaJohn <victorsabba@gmail.com>
2023-07-15 19:03:09 -0300

    Lets do the SOAP shit

---
+++ manage.py
@@ -0,0 +1,17 @@
+from werkzeug.middleware.dispatcher import DispatcherMiddleware
+
+from spyne.server.wsgi import WsgiApplication
+
+from apps.spyne import spyned
+from apps.flask_app.flasked import app
+
+
+# SOAP services are distinct wsgi applications, we should use dispatcher
+# middleware to bring all aps together
+app.app.wsgi_app = DispatcherMiddleware(app.app.wsgi_app, {
+    '/soap': WsgiApplication(spyned.create_app(app))
+})
+
+
+if __name__ == '__main__':
+    app.run()
